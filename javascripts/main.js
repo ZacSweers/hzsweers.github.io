cfd4ab34fa7184407bf15ea30c0ce646e250f39a
Henri Sweers <pandanomic@gmail.com>
2013-12-05 06:58:43 -0600

    Info

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,3 @@
 console.log('This would be the main JS file.');
+<link rel="stylesheet" href="http://yandex.st/highlightjs/7.5/styles/default.min.css">
+<script src="http://yandex.st/highlightjs/7.5/highlight.min.js"></script>
